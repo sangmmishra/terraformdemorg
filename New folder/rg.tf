3a9325fee08c7d4c26a86da13338576ab0781a09
subbaiah <subbaiah@97>
2022-09-21 13:27:42 +0000

    modified folder

--- New folder/rg.tf
+++ New folder/rg.tf
@@ -5,3 +5,15 @@ tags =  {
            "evn" = "dev"
 }
 }
+
+resource "azurerm_virtual_network" "vnet" {
+name = "ventrg"
+location = azurerm_resource_group.rg.lcation
+resource_group_name = azurerm_resource_group.rg.name
+adress_space = 10.0.0.0/16
+
+subnet {
+name = "subnet1"
+adress_prefix = "10.1.0.0/24
+}
+}
