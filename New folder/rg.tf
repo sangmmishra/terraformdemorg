c09521f57016a8b3ba6b809a4665b7851e0384c3
subbaiah <subbaiah@97>
2022-09-21 16:21:21 +0000

    folder2

--- New folder/rg.tf
+++ New folder/rg.tf
@@ -5,15 +5,16 @@ tags =  {
            "evn" = "dev"
 }
 }
-
 resource "azurerm_virtual_network" "vnet" {
-name = "ventrg"
-location = azurerm_resource_group.rg.lcation
-resource_group_name = azurerm_resource_group.rg.name
-adress_space = 10.0.0.0/16
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-subnet {
-name = "subnet1"
-adress_prefix = "10.0.1.0/24"
-}
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
 }
