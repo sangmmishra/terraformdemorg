0a36af85708358d5ed307fa462241a552ef5e0c3
subbaiah <subbaiah@97>
2022-09-21 16:26:10 +0000

    1folders3

--- New folder/rg.tf
+++ New folder/rg.tf
@@ -7,8 +7,8 @@ tags =  {
 }
 resource "azurerm_virtual_network" "vnet" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
