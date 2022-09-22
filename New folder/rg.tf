6051434cc3b25a08149411603586758535fe35fc
sangam <110962434+sangmmishra@users.noreply.github.com>
2022-09-22 13:36:04 +0000

    Update rg.tf

--- New folder/rg.tf
+++ New folder/rg.tf
@@ -9,12 +9,12 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "example-network"
   location            = azurerm_resource_group.rg.lcation
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = 10.0.0.0/16
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
   subnet {
     name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
+    address_prefix = "10.0.1.0/24
   }
 
 }
