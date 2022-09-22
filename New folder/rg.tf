70eb6d9f71140676d2bb3e8e6141e33a28b7135d
sangam <110962434+sangmmishra@users.noreply.github.com>
2022-09-22 13:30:05 +0000

    Update rg.tf

--- New folder/rg.tf
+++ New folder/rg.tf
@@ -18,3 +18,13 @@ resource "azurerm_virtual_network" "vnet" {
   }
 
 }
+resource "azurerm_public_ip" "public" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+}
